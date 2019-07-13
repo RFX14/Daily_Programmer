06f3cb2b531d9ae69374c31158286127a406c0dc
Josue Rosales <jrosa1756@gmail.com>
2019-07-13 17:55:23 -0500

    More game logic
    Implemented a bit more game logic:
        -Flip Cards
        -Remove Cards
        -End a game when there are no cards left
    Issues:
        -There are no empty spaces between cards, all cards are pushed closer together which doesn't follow the proper game rules
            -I didn't realize that there had to be space inbetween cards until I started watching the video that is in the Intermediate_README

--- #375/Intermediate.cpp
+++ #375/Intermediate.cpp
@@ -2,35 +2,85 @@
 #include <vector>
 #include <cstring>
 
-int main(int argc, char* argv[]) {
-    std::string input = argv[1];
-    std::vector<char> cards(input.begin(), input.end());
-    std::vector<int> usableCards{};
+void flip(std::vector<bool>& vect, int idx) {
+    if(idx == 0) {
+        vect.at(idx + 1) = !vect.at(idx + 1);
+    } else if(idx == vect.size() - 1) {
+        vect.at(idx - 1) = !vect.at(idx - 1);
+    } else {
+        vect.at(idx - 1) = !vect.at(idx - 1);
+        vect.at(idx + 1) = !vect.at(idx + 1);
+    }
+}
 
+void updateUsable(std::vector<bool> cards, std::vector<int>& vect) {
+    vect.clear();
     for(int i = 0; i < cards.size(); i++) {
-        if(cards.at(i) == '1') {
-            usableCards.push_back(i);
+        if(cards.at(i) == 1) {
+            vect.push_back(i);
         }
     }
+}
 
-    bool solved{true};
-    //while(!solved) {
-        std::cout << "Choose a card to remove! \n";
+//Converts a vect<int> to vect<bool> assuming the vect<int> is made of ASCII values of chars(48 = '0' & 49 = '1')
+std::vector<bool> convert(std::vector<int> vect) {
+    std::vector<bool> vectFinal{};
+    for(int i = 0; i < vect.size(); i++) {
+        bool temp = (vect.at(i) == 49)? true : false;
+        vectFinal.push_back(temp);
+    }
+    return vectFinal;
+}
+
+int main(int argc, char* argv[]) {
+    std::string input = argv[1];
+    std::vector<int> cardsInput(input.begin(), input.end());
+    std::vector<bool> cards{convert(cardsInput)};
+    std::vector<int> usableCards{};
+
+    bool solved{false};
+    while(!solved) {
+        std::cout << '\n';
+
+        updateUsable(cards, usableCards);
+
+        //Prints out current state of cards
+        for(int i = 0; i < cards.size(); i++) {
+            std::cout << cards.at(i) << " ";
+        }
+
+        std::cout << "\nChoose a card to remove!\n";
         std::cout << "Options: ";
 
+        //Prints out options that user can choose
         for(int i = 0; i < usableCards.size(); i++) {
             std::cout << '[' << usableCards.at(i) << "] ";
         }
-
-        std::cout << "\n>> ";
+        
         int removeIdx{};
+        std::cout << "\n>> ";
         std::cin >> removeIdx;
 
-        cards.erase(cards.begin() + removeIdx);
-        //usableCards.erase(usableCards.begin() + );
+        try {
+            flip(cards, removeIdx);
+            cards.erase(cards.begin() + removeIdx);
+        } catch(std::out_of_range) {
+            solved = true;
+        }
 
-        
-    //}
+        //Removes an option from the options stack and renumbers options if necassry
+        bool found{false};
+        for(int i = 0; i < usableCards.size(); i++) {
+            if(usableCards.at(i) == removeIdx) { 
+                removeIdx = i; 
+                found = true;
+            }
+            if(found) { usableCards.at(i)--; }
+        }
+
+        usableCards.erase(usableCards.begin() + removeIdx);
+ 
+    }
 
     return 0;
 }
